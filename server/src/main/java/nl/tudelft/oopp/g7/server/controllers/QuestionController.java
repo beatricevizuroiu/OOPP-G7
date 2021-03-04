e4a077126ba30daa45e2ac2122f152714df998c9
Kalle Struik <K.H.Struik@student.tudelft.nl>
2021-03-04 13:45:58 +0100

    Add delete endpoint for deleting questions.

--- server/src/main/java/nl/tudelft/oopp/g7/server/controllers/QuestionController.java
+++ server/src/main/java/nl/tudelft/oopp/g7/server/controllers/QuestionController.java
@@ -6,20 +6,18 @@ import nl.tudelft.oopp.g7.common.Question;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-@Controller()
+@RestController()
 @RequestMapping("/api/v1/question")
 public class QuestionController {
 
     //TODO: Probably want to move the database table creation somewhere else.
-    //TODO: Once switched to postgresql make postedAt default to the current date.
-    private static final String QUERY_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS QUESTIONS("
+    private static final String QUERY_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS questions("
             + "id serial PRIMARY KEY not NULL,"
             + "text text not NULL,"
             + "answer text DEFAULT '' not NULL,"
@@ -32,6 +30,7 @@ public class QuestionController {
     private static final String QUERY_SELECT_ALL_QUESTIONS = "SELECT * FROM questions";
     private static final String QUERY_CREATE_QUESTION = "INSERT INTO questions (text) VALUES (?)";
     private static final String QUERY_ANSWER_QUESTION = "UPDATE questions SET answer=?, answered=true WHERE id=?";
+    private static final String QUERY_DELETE_QUESTION = "DELETE FROM questions WHERE id=?";
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -47,7 +46,6 @@ public class QuestionController {
      *         if there is no question found it will return an empty {@link ResponseEntity} with http status 404 (NOT_FOUND).
      */
     @GetMapping("/{id}")
-    @ResponseBody
     public ResponseEntity<Question> getQuestion(@PathVariable("id") int id) {
         // Get the question with the id from the database.
         Question question = jdbcTemplate.query(QUERY_SELECT_QUESTION_BY_ID,
@@ -73,7 +71,6 @@ public class QuestionController {
      * @return A {@link List} of {@link Question}s containing every question in the database.
      */
     @GetMapping("/all")
-    @ResponseBody
     public List<Question> getAllQuestions() {
         // Return every question in the database.
         return jdbcTemplate.query(QUERY_SELECT_ALL_QUESTIONS, (rs) -> {
@@ -90,11 +87,31 @@ public class QuestionController {
     }
 
     /**
+     * Endpoint to delete questions.
+     * @param id The id of the question to delete.
+     * @return A {@link ResponseEntity} containing NULL and a status code of 200 (OK) if a question was delete and 404
+     *      (NOT_FOUND) if no question was deleted.
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteQuestion(@PathVariable("id") int id) {
+        // Try to delete the question with id and store the amount of rows changed.
+        int rowsChanged = jdbcTemplate.update(QUERY_DELETE_QUESTION, (ps) -> ps.setInt(1, id));
+
+        // Check if there was a question deleted.
+        if (rowsChanged == 1) {
+            // If there was return http status code 200 (OK)
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } else {
+            // Otherwise return http status code 404 (NOT_FOUND)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
      * Endpoint to create a new question.
      * @param newQuestion The {@link NewQuestion} object representing the new question.
      */
     @PostMapping("/new")
-    @ResponseBody
     public void newQuestion(@RequestBody NewQuestion newQuestion) {
         // Create a new question in the database.
         jdbcTemplate.update(QUERY_CREATE_QUESTION,
@@ -110,7 +127,6 @@ public class QuestionController {
      * @return A {@link ResponseEntity} containing NULL and a http status of 200 (OK) if a row is changed and 404 (NOT_FOUND) if no rows changed.
      */
     @PostMapping("/{id}/answer")
-    @ResponseBody
     public ResponseEntity<Void> answerQuestion(@PathVariable int id, @RequestBody Answer answer) {
         // Update the question with the answer and store the amount of rows changed in a variable.
         int rowsChanged = jdbcTemplate.update(QUERY_ANSWER_QUESTION,
@@ -126,7 +142,7 @@ public class QuestionController {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
 
-        // Otgherwise respond with http status code 200 (OK).
+        // Otherwise respond with http status code 200 (OK).
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
