fdc4ad4333a8c04cf93668050e0014d95550c640
Mehmet Alp Sozuduz <M.A.Sozuduz@student.tudelft.nl>
2021-03-13 15:01:34 +0100

    Remove remnant quote files.

--- server/src/main/java/nl/tudelft/oopp/g7/server/repositories/QuoteRepository.java
+++
@@ -1,6 +0,0 @@
-package nl.tudelft.oopp.g7.server.repositories;
-
-import nl.tudelft.oopp.g7.server.entities.Quote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuoteRepository extends JpaRepository<Quote, Long> {}
