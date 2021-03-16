7c990419d6668fdbb0675797f12d4dcab2fd22b9
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-16 23:47:28 +0100

    alert message for when you create a room

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
@@ -1,9 +1,16 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import nl.tudelft.oopp.g7.client.communication.ServerCommunication;
 
 public class createRoomController {
+    @FXML
+    TextField roomName;
+    @FXML
+    TextField lecturerName;
+
     /**
      * Handles clicking the button.
      */
@@ -11,7 +18,8 @@ public class createRoomController {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Room created");
         alert.setHeaderText(null);
-        alert.setContentText("Your room is created");
+        alert.setContentText("You are creating the room for Course: " + roomName.getText() +
+                " held by lecturer: " + lecturerName.getText() + ".\nDo you want to continue?");
         alert.showAndWait();
     }
 }
