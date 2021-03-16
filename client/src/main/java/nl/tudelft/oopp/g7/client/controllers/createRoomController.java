0b6829f1d65d9e5c1445d5cd0a224be5bbeb9c37
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-16 22:46:35 +0100

    set right controller for each class

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
@@ -1,4 +1,18 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.scene.control.Alert;
+import nl.tudelft.oopp.g7.client.communication.ServerCommunication;
+
 public class createRoomController {
+
+    /**
+     * Handles clicking the button.
+     */
+    public void buttonClicked() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Quote for you");
+        alert.setHeaderText(null);
+        alert.setContentText(ServerCommunication.getQuote());
+        alert.showAndWait();
+    }
 }
