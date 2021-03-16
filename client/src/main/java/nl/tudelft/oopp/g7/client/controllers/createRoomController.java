5c3fb2c96aa75df18ba43ea1e746ca6d63439f5a
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-16 23:15:20 +0100

    added alert to tell the user that the room has been created

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
@@ -4,15 +4,14 @@ import javafx.scene.control.Alert;
 import nl.tudelft.oopp.g7.client.communication.ServerCommunication;
 
 public class createRoomController {
-
     /**
      * Handles clicking the button.
      */
     public void buttonClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Quote for you");
+        alert.setTitle("Room created");
         alert.setHeaderText(null);
-        alert.setContentText(ServerCommunication.getQuote());
+        alert.setContentText("Your room is created");
         alert.showAndWait();
     }
 }
--- client/src/main/java/nl/tudelft/oopp/g7/client/views/CreateRoomDisplay.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/views/CreateRoomDisplay.java
@@ -1,19 +1,20 @@
 package nl.tudelft.oopp.g7.client.views;
 
-import java.io.IOException;
-import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class QuoteDisplay extends Application {
+import java.io.IOException;
+import java.net.URL;
 
+public class CreateRoomDisplay extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
-        URL xmlUrl = getClass().getResource("/mainScene.fxml");
+        URL xmlUrl = getClass().getResource("/createRoom.fxml");
         loader.setLocation(xmlUrl);
         Parent root = loader.load();
 
