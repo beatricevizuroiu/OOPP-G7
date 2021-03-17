3f9691c66751b366311b6deda9aa503d4e88d11d
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-17 14:19:14 +0100

    added TextArea to send the Questions as student and created the sendQuestion Method

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/StudentViewUIController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/StudentViewUIController.java
@@ -1,10 +1,19 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import nl.tudelft.oopp.g7.client.views.EntryRoomDisplay;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+import nl.tudelft.oopp.g7.client.communication.StudentServerCommunication;
+import nl.tudelft.oopp.g7.common.QuestionText;
 
 public class StudentViewUIController {
+    public ListView<String> listView = new ListView<>();
+    public TextArea answerBox;
 
+    // TODO: ListView
+
+    public void sendQuestion() {
+        StudentServerCommunication.askQuestion(new QuestionText(answerBox.getText()));
+        answerBox.setText("");
+    }
 }
