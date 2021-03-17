92324a89cfd05889e61468c41ec5b258ae707319
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-17 14:11:56 +0100

    added LecturerView and go to LecturerView when clicked Create

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/createRoomController.java
@@ -1,9 +1,13 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import nl.tudelft.oopp.g7.client.communication.ServerCommunication;
+import nl.tudelft.oopp.g7.client.views.EntryRoomDisplay;
 
 public class createRoomController {
     @FXML
@@ -20,6 +24,20 @@ public class createRoomController {
         alert.setHeaderText(null);
         alert.setContentText("You are creating the room for Course: " + roomName.getText() +
                 " held by lecturer: " + lecturerName.getText() + ".");
+
+        ButtonType okButton = new ButtonType ("OK");
+        ButtonType cancelButton = new ButtonType ("Cancel");
+
+        alert.getButtonTypes().setAll(okButton, cancelButton);
+
         alert.showAndWait();
+
+        // if the user presses OK, the go to Student View
+        if (alert.getResult() == okButton){
+            Scene scene = EntryRoomDisplay.getCurrentScene();
+            Stage stage = EntryRoomDisplay.getCurrentStage();
+
+            EntryRoomDisplay.setCurrentScene("/LecturerViewUI.fxml");
+        }
     }
 }
