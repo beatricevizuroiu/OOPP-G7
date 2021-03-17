ec160aa778ba78711cb3601b14f8e38fab90d18c
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-17 12:25:10 +0100

    EntryPage controller and display to change scenes when buttons are pressed

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/entryPageController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/entryPageController.java
@@ -1,4 +1,40 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.g7.client.views.EntryRoomDisplay;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class entryPageController {
+    @FXML
+    private Button createRoomButton;
+    @FXML
+    private Label label;
+//    @FXML
+//    private Button joinRoomButton;
+
+    public void handleButtonAction(ActionEvent event) {
+        Scene scene = EntryRoomDisplay.getCurrentScene();
+        Stage stage = EntryRoomDisplay.getCurrentStage();
+
+        EntryRoomDisplay.setCurrentScene("/createRoom.fxml");
+    }
+
+    public void handleButtonAction2(ActionEvent event) {
+        Scene scene = EntryRoomDisplay.getCurrentScene();
+        Stage stage = EntryRoomDisplay.getCurrentStage();
+
+        EntryRoomDisplay.setCurrentScene("/joinRoom.fxml");
+    }
 }
---
+++ client/src/main/java/nl/tudelft/oopp/g7/client/views/EntryRoomDisplay.java
@@ -0,0 +1,73 @@
+package nl.tudelft.oopp.g7.client.views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class EntryRoomDisplay extends Application {
+
+    private static Stage currentStage;
+    private static Scene currentScene;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+        currentStage = primaryStage;
+        FXMLLoader loader = new FXMLLoader();
+
+        URL xmlUrl = getClass().getResource("/entryPage.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        currentScene = new Scene(root);
+
+        currentStage.setScene(currentScene);
+        currentStage.show();
+    }
+
+
+    /**
+     * Get current scene scene.
+     *
+     * @return the scene
+     */
+    public static Scene getCurrentScene(){
+        return currentScene;
+    }
+
+    /**
+     * Set current scene.
+     *
+     * @param newSceneName the new scene name
+     */
+    public static void setCurrentScene(String newSceneName){
+        try{
+            Parent parent = FXMLLoader.load(EntryRoomDisplay.class.getResource(newSceneName));
+            Scene newScene = new Scene(parent);
+
+            currentStage.setScene((newScene));
+            EntryRoomDisplay.currentScene = newScene;
+
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get current stage stage.
+     *
+     * @return the stage
+     */
+    public static Stage getCurrentStage(){
+        return currentStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
