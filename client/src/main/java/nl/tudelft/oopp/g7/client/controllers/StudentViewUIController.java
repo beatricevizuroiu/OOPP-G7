7fc32afd05a76ca4cfde63c9ec479b3c0d553e6a
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-17 14:50:47 +0100

    commented retrieveQuestions method - help needed

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/StudentViewUIController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/StudentViewUIController.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
@@ -10,10 +11,16 @@ public class StudentViewUIController {
     public ListView<String> listView = new ListView<>();
     public TextArea answerBox;
 
-    // TODO: ListView
+
+//    public void retrieveQuestions() {
+//        StudentServerCommunication.retrieveAllQuestions();
+//        listView.getItems();
+//    }
 
     public void sendQuestion() {
         StudentServerCommunication.askQuestion(new QuestionText(answerBox.getText()));
-        answerBox.setText("");
+        answerBox.setText("Type your question here");
     }
+
+
 }
