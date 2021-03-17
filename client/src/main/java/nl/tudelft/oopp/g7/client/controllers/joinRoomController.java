57e99672abf9e3c79edbcd6efacc62f6486f4f96
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-03-17 12:34:58 +0100

    join room display and controller added

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/joinRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/joinRoomController.java
@@ -1,4 +1,29 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class joinRoomController {
+    @FXML
+    TextField nickname;
+    @FXML
+    TextField roomId;
+    @FXML
+    TextField roomPassword;
+
+    /**
+     * Handles clicking the button.
+     */
+    public void buttonClicked() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Create Room");
+        alert.setHeaderText(null);
+        if(nickname.getText() != null){
+            alert.setContentText("You are joining room: " + roomId.getText() + " as: " + nickname.getText());
+        }else {
+            alert.setContentText("You are joining room: " + roomId.getText());
+        }
+        alert.showAndWait();
+    }
 }
