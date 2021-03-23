02b57c3d8d2cb06c3c48325a5458f1e385e584bd
Elena <e.martellucci@student.tudelft.nl>
2021-03-23 14:33:52 +0100

    Controller for Answered Questions

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/AnsweredQuestionsController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/AnsweredQuestionsController.java
@@ -1,4 +1,51 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.g7.client.views.EntryRoomDisplay;
+
 public class AnsweredQuestionsController {
+
+/*    *//**
+     * Handle button action for button Mode from Light.
+     *
+     * @param event the event
+     *//*
+    public void handleButtonMode(ActionEvent event) {
+        Scene scene = EntryRoomDisplay.getCurrentScene();
+        Stage stage = EntryRoomDisplay.getCurrentStage();
+
+        // if Mode is clicked, change Scene to Join Room
+        EntryRoomDisplay.setCurrentScene("/AnsweredQuestions(DARKMODE).fxml");
+    }
+
+    *//**
+     * Handle button action for button Mode from Dark.
+     *
+     * @param event the event
+     *//*
+    public void handleButtonMode2(ActionEvent event) {
+        Scene scene = EntryRoomDisplay.getCurrentScene();
+        Stage stage = EntryRoomDisplay.getCurrentStage();
+
+        // if Mode is clicked, change Scene to Join Room
+        EntryRoomDisplay.setCurrentScene("/AnsweredQuestions.fxml");
+    }
+
+    public void handleHelpButtonLight(ActionEvent event) {
+        Scene scene = EntryRoomDisplay.getCurrentScene();
+        Stage stage = EntryRoomDisplay.getCurrentStage();
+
+        // if Help is clicked, change to Help scene
+        EntryRoomDisplay.setCurrentScene("/AnsweredQuestions.fxml");
+    }
+
+    public void handleHelpButtonDark(ActionEvent event) {
+        Scene scene = EntryRoomDisplay.getCurrentScene();
+        Stage stage = EntryRoomDisplay.getCurrentStage();
+
+        // if Help is clicked, change to Help scene
+        EntryRoomDisplay.setCurrentScene("/AnsweredQuestions(DARKMODE).fxml");
+    }*/
 }
