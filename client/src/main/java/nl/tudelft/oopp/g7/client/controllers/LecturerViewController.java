ddf2e58bdb7209758ffadd4a3d8c742122eb2f41
Kalle Struik <k.h.struik@student.tudelft.nl>
2021-03-29 13:23:42 +0200

    Add answered questions to the mod views.

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/LecturerViewController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/LecturerViewController.java
@@ -44,7 +44,7 @@ public class LecturerViewController {
         System.out.println(roomID);
 
         // Start a timer and create a separate thread on it to automatically call retrieveQuestions()
-        Timer timer = new Timer();
+        Timer timer = new Timer(true);
 
         LecturerViewController reference = this;
         timer.schedule(new TimerTask() {
