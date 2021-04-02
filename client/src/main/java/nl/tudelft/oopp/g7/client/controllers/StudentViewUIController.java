13e952bb41bdec359d851992d7ee4374e5ee1cc2
Beatrice Andreea Vizuroiu <b.vizuroiu@student.tudelft.nl>
2021-04-02 13:33:21 +0200

    slower & faster buttons + changing color when pressed

--- client/src/main/java/nl/tudelft/oopp/g7/client/controllers/StudentViewUIController.java
+++ client/src/main/java/nl/tudelft/oopp/g7/client/controllers/StudentViewUIController.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.g7.client.controllers;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import nl.tudelft.oopp.g7.client.communication.StudentServerCommunication;
 import nl.tudelft.oopp.g7.client.logic.LocalData;
 import nl.tudelft.oopp.g7.client.logic.StudentViewLogic;
@@ -33,6 +35,12 @@ public class StudentViewUIController {
     // area to type the question
     @FXML
     public TextArea answerBox;
+
+    @FXML
+    public Button colorSlow;
+
+    @FXML
+    public Button colorFast;
 
     private final String roomID;
     private final String nickname;
@@ -209,5 +217,25 @@ public class StudentViewUIController {
         //TODO
     }
 
+    /**
+     * Handle button slower.
+     */
+    public void goSlower() {
+        colorSlow.setVisible(true);
+        PauseTransition transition = new PauseTransition(Duration.seconds(3));
+        transition.setOnFinished(event -> colorSlow.setVisible(false));
 
+        transition.play();
+    }
+
+    /**
+     * Handle button faster.
+     */
+    public void goFaster() {
+        colorFast.setVisible(true);
+        PauseTransition transition = new PauseTransition(Duration.seconds(3));
+        transition.setOnFinished(event -> colorFast.setVisible(false));
+
+        transition.play();
+    }
 }
