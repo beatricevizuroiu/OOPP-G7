0e3f3e43229044b4f899a58114f536e6e52d1694
Elena <e.martellucci@student.tudelft.nl>
2021-04-08 16:59:13 +0200

    Fix server build.

--- server/src/main/java/nl/tudelft/oopp/g7/server/controllers/RoomController.java
+++ server/src/main/java/nl/tudelft/oopp/g7/server/controllers/RoomController.java
@@ -174,7 +174,7 @@ public class RoomController {
     /**
      * Close the currently running poll.
      * @param roomId The id of the room to close the poll in.
-     * @param RoomCloseRequest The {@link PollCloseRequest} to close the poll from.
+     * @param RoomCloseRequest The {@link RoomCloseRequest} to close the room from.
      * @param authorization The authorization header.
      * @param request The {@link HttpServletRequest} associated with the Http request.
      * @return A {@link ResponseEntity} containing the http status code indicating whether the request completed
@@ -200,7 +200,7 @@ public class RoomController {
         roomRepository.endRoom(roomId);
 
         return new ResponseEntity<>(HttpStatus.OK);
-
+    }
 
         /**
          * Endpoint to change the Speed of a Room.
